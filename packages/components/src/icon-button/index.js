aduth/gutenberg

commit 14374e5f08ff7cd91d281cbfc4102cec39d45ed8

Set a default icon margin for buttons with icon and text. (#12901)

packages/components/src/icon-button/index.js
@@ -22,7 +22,9 @@ class IconButton extends Component {
 	render() {
 		const { icon, children, label, className, tooltip, shortcut, labelPosition, ...additionalProps } = this.props;
 		const { 'aria-pressed': ariaPressed } = this.props;
-		const classes = classnames( 'components-icon-button', className );
+		const classes = classnames( 'components-icon-button', className, {
+			'has-text': children,
+		} );
 		const tooltipText = tooltip || label;
 
 		// Should show the tooltip if...

packages/block-library/src/audio/test/__snapshots__/index.js.snap
@@ -38,7 +38,7 @@ exports[`core/audio block edit matches snapshot 1`] = `
       class="components-form-file-upload"
     >
       <button
-        class="components-button components-icon-button editor-media-placeholder__button is-button is-default is-large"
+        class="components-button components-icon-button editor-media-placeholder__button has-text is-button is-default is-large"
         type="button"
       >
         <svg

packages/block-library/src/cover/test/__snapshots__/index.js.snap
@@ -38,7 +38,7 @@ exports[`core/cover block edit matches snapshot 1`] = `
       class="components-form-file-upload"
     >
       <button
-        class="components-button components-icon-button editor-media-placeholder__button is-button is-default is-large"
+        class="components-button components-icon-button editor-media-placeholder__button has-text is-button is-default is-large"
         type="button"
       >
         <svg

packages/block-library/src/gallery/test/__snapshots__/index.js.snap
@@ -38,7 +38,7 @@ exports[`core/gallery block edit matches snapshot 1`] = `
       class="components-form-file-upload"
     >
       <button
-        class="components-button components-icon-button editor-media-placeholder__button is-button is-default is-large"
+        class="components-button components-icon-button editor-media-placeholder__button has-text is-button is-default is-large"
         type="button"
       >
         <svg

packages/block-library/src/video/test/__snapshots__/index.js.snap
@@ -38,7 +38,7 @@ exports[`core/video block edit matches snapshot 1`] = `
       class="components-form-file-upload"
     >
       <button
-        class="components-button components-icon-button editor-media-placeholder__button is-button is-default is-large"
+        class="components-button components-icon-button editor-media-placeholder__button has-text is-button is-default is-large"
         type="button"
       >
         <svg

packages/components/src/icon-button/style.scss
@@ -19,10 +19,10 @@
 	svg {
 		fill: currentColor;
 		outline: none;
+	}
 
-		&:not:only-child {
-			margin-right: 4px;
-		}
+	&.has-text svg {
+		margin-right: 4px;
 	}
 
 	&:not(:disabled):not([aria-disabled="true"]):not(.is-default):hover {

packages/edit-post/src/components/header/plugin-more-menu-item/test/__snapshots__/index.js.snap
@@ -18,7 +18,7 @@ exports[`PluginMoreMenuItem renders menu item as button properly 1`] = `
   >
     <button
       aria-label="My plugin button menu item"
-      className="components-button components-icon-button components-menu-item__button has-icon"
+      className="components-button components-icon-button components-menu-item__button has-icon has-text"
       onClick={[Function]}
       role="menuitem"
       type="button"

commit 9e7119ef7402601fecfbaa38b6a26375cafd42de

Fix icon size regression in Switcher
In #12901, a small margin was introduced to the IconButton component when text was present. This caused an issue with block icons, scaling them down when they shouldn't be. A 20x20px icon should show as 20x20, and a 24x24px icon should show as 24x24px.

The additional margin was applied even when the IconButton didn't actually have text. It simply needed `children`, and in the case of the Switcher, it has a dropdown arrow. Combined with the fixed width of the switcher, that meant a 24x24 icon would be rendered as 20x24.

This PR adds a length check so if your IconButton includes more than 2 letters, it's considered to have a text label. This fixes the issue. But as you can tell, maybe it's not the best way to check whether there's text or not. Suggestions most welcome.

packages/components/src/icon-button/index.js
@@ -23,7 +23,7 @@ class IconButton extends Component {
 		const { icon, children, label, className, tooltip, shortcut, labelPosition, ...additionalProps } = this.props;
 		const { 'aria-pressed': ariaPressed } = this.props;
 		const classes = classnames( 'components-icon-button', className, {
-			'has-text': children,
+			'has-text': children && children.length > 2,
 		} );
 		const tooltipText = tooltip || label;
 